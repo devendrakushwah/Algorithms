6499cfc3be3fee36099e33a3ce7aaaab2cdaffa2
Devendra Kushwah <idevendra08@gmail.com>
2018-08-27 14:40:56 +0530

    update

---
+++ SieveOfEratosthenes.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+/*
+Primarility check using Sieve of Eratosthenes.
+Generate prime numbers upto n*/
+int main()
+{
+    int n=pow(10,6);
+    bool isPrime[n];
+    memset(isPrime, true, sizeof(isPrime));
+    for(int i=2;i<=n;i++)
+    {
+        if(isPrime[i] == true)
+        {
+            for(int j=2*i; j<=n; j=j+i)
+            {
+                isPrime[j]=false;
+            }
+        }
+    }
+    printf("%d",isPrime[2]);
+    printf("%d",isPrime[33]);
+    printf("%d",isPrime[41]);
+    printf("%d",isPrime[99]);
+    return 0;
+}
