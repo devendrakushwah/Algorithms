ac06b71c5663710a95697849af81a91b16d1f214
Devendra Kushwah <idevendra08@gmail.com>
2019-01-05 22:30:23 +0530

    refactored

--- kadane's.py
+++ kadane's.py
@@ -2,12 +2,10 @@
 def kadane(a,n):
     max_sum = a[0]
     temp_sum = a[0]
-    
     for i in range(0,n): 
         temp_sum = max(a[i],temp_sum+a[i]) 
         max_sum=max(temp_sum,max_sum)
     return max_sum
-  
 
 #main
 print(kadane([1,8,-2,-9,4,13,-2,1],8))
