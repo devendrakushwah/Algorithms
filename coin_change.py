5305f4a1628e2aee12dce86eaf1d00af4e266b36
Devendra Kushwah <idevendra08@gmail.com>
2019-01-14 22:36:27 +0530

    update

--- coin_change.py
+++ coin_change.py
@@ -1,10 +1,13 @@
 def count_ways(s,m,n):
+  #s= list of denominations
+  #m= no. of denominations
+  #n= amount of money to be obtained
   '''Space Complexity of this function: O(n) 
   table[i] will be storing the number of solutions
   for value i. We need n+1 rows as the table is
   constructed in bottom up manner using the base
   case (n = 0)'''
-
+  
   # Initialize all table values as 0 
   table=[0]*(n+1) #takes O(n)
 
